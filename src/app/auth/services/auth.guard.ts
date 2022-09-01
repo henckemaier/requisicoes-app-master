f203d3ebca0f6661c81367e3bf2b68b00c10eaa2
Eduardo Henckemaier <eduhenckemaier@outlook.com>
2022-09-01 16:17:11 -0300

    ; faltando no login e auth guard

--- src/app/auth/services/auth.guard.ts
+++ src/app/auth/services/auth.guard.ts
@@ -24,7 +24,7 @@ export class AuthGuard implements CanActivate {
           this.router.navigate(["/login"]);
           return false;
         })
-      )
+      );
   }
 
 }
--- src/app/auth/services/login.guard.ts
+++ src/app/auth/services/login.guard.ts
@@ -23,6 +23,6 @@ export class LoginGuard implements CanActivate {
           this.router.navigate(["/painel"]);
           return false;
         })
-      )
+      );
   }
 }
