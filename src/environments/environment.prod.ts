a1bf142d4eb44ac4d762b60102b9d304ef8fd2e6
Eduardo Henckemaier <eduhenckemaier@outlook.com>
2022-09-13 15:37:52 -0300

    atualizado o enviroments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDiZGoA7CUBnp85CNty6oV_sMsvQOWZuho",
+    authDomain: "requisicoes-app-2bd57.firebaseapp.com",
+    projectId: "requisicoes-app-2bd57",
+    storageBucket: "requisicoes-app-2bd57.appspot.com",
+    messagingSenderId: "426728699779",
+    appId: "1:426728699779:web:e347210336b133bf44a8b6"
+  }
 };
